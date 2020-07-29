d9bc6f4872eeaa9eafd220c4a3868b5673f11708
Hồng Thất Công <bangchucaibang.htc@gmail.com>
2020-07-29 08:31:06 +0700

    Initial commit

---
+++ diaphora_load.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+"""
+Diaphora, a diffing plugin for IDA
+Copyright (c) 2015-2019, Joxean Koret
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
+import diaphora_ida
+import imp
+
+imp.reload(diaphora_ida)
+
+
+# -----------------------------------------------------------------------
+def main():
+    diaphora_ida.load_results()
+
+
+if __name__ == "__main__":
+    main()
